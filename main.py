781e9b1acff73fda6d28638fd5e10b892df533b4
mehmet-mhy <mehmet.mhy@gmail.com>
2024-04-09 04:53:02 -0600

    started gemeni api converting

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
-# os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+llm = ChatGoogleGenerativeAI(
+    model="gemini-pro",
+    convert_system_message_to_human=True,
+    verbose=True,
+    temperature=0.1,
+)
 
 search_tool = SerperDevTool()
 
@@ -16,43 +21,40 @@ researcher = Agent(
   You have a knack for dissecting complex data and presenting actionable insights.""",
   verbose=True,
   allow_delegation=False,
+  llm=llm,
   tools=[search_tool]
-  # You can pass an optional llm attribute specifying what mode you wanna use.
-  # It can be a local model through Ollama / LM Studio or a remote
-  # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
-  #
-  # import os
-  # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
-  #
-  # OR
-  #
-  # from langchain_openai import ChatOpenAI
-  # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
 )
+
 writer = Agent(
   role='Tech Content Strategist',
   goal='Craft compelling content on tech advancements',
   backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
   You transform complex concepts into compelling narratives.""",
   verbose=True,
-  allow_delegation=True
+  allow_delegation=True,
+  llm=llm,
 )
 
 # Create tasks for your agents
 task1 = Task(
-  description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
-  Identify key trends, breakthrough technologies, and potential industry impacts.""",
-  expected_output="Full analysis report in bullet points",
-  agent=researcher
+    description=(
+        "Conduct a comprehensive analysis of the latest advancements in AI in 2024."
+        "Identify key trends, breakthrough technologies, and potential industry impacts."
+        "Compile your findings in a detailed report."
+        "Make sure to check with a human if the draft is good before finalizing your answer."
+    ),
+    expected_output="A comprehensive full report on the latest AI advancements in 2024, leave nothing out",
+    agent=researcher,
 )
 
 task2 = Task(
-  description="""Using the insights provided, develop an engaging blog
-  post that highlights the most significant AI advancements.
-  Your post should be informative yet accessible, catering to a tech-savvy audience.
-  Make it sound cool, avoid complex words so it doesn't sound like AI.""",
-  expected_output="Full blog post of at least 4 paragraphs",
-  agent=writer
+    description=(
+        "Using the insights from the researcher's report, develop an engaging blog post that highlights the most significant AI advancements."
+        "Your post should be informative yet accessible, catering to a tech-savvy audience."
+        "Aim for a narrative that captures the essence of these breakthroughs and their implications for the future."
+    ),
+    expected_output="A compelling 3 paragraphs blog post formatted as markdown about the latest AI advancements in 2024",
+    agent=writer,
 )
 
 # Instantiate your crew with a sequential process
