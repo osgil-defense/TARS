70556e109fee39eef0a4b3a16948b0a121f03dba
mehmet-mhy <mehmet.mhy@gmail.com>
2024-04-15 03:20:18 -0600

    updated search_data() code with new algo and removed it's test code

--- py_main/tools.py
+++ py_main/tools.py
@@ -2,6 +2,7 @@ from langchain_community.tools import DuckDuckGoSearchRun
 import subprocess
 from subprocess import Popen, PIPE
 from crewai_tools import tool
+import datetime
 import requests
 import json
 import os
@@ -40,42 +41,78 @@ def get_past_week_data():
 
 
 @tool("NotifyCyberSearch")
-def search_data(queries: list):
+def search_data(queries: list, n: int = 10):
     """
-    Search for key words in a cybersecurity news database hosted by Notify Cyber
+    Search for key words in a cybersecurity news database hosted by Notify Cyber.
 
     Parameters:
         queries (list of str): List of keywords to search for from the API.
+        n (int): Number of search results to return.
 
     Raises:
-        ValueError: If the NC_API_TOKEN environment variable is not set, if queries is not a list of strings, or if any query is not a string.
+        ValueError: If queries is not a list of strings, or if any query is not a string.
+        ValueError: If n is not an integer or is less than 1.
         Exception: If the API request fails.
 
     Returns:
         JSON response from the API.
     """
-    if not isinstance(queries, list) or not all(
-        isinstance(query, str) for query in queries
-    ):
+    if not isinstance(queries, list) or not all(isinstance(query, str) for query in queries):
         raise ValueError("Queries must be a list of strings")
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be an integer greater than 0")
 
     auth_token = os.getenv("NC_API_TOKEN")
     if not auth_token:
         raise ValueError("NC_API_TOKEN environment variable is not set")
 
-    # Joining the list of queries into a single string separated by commas
-    query_string = ",".join(queries)
-    url = f"https://nc-api.vercel.app/search?{query_string}"
-    response = requests.get(url, headers={"Authorization": auth_token})
-    if response.ok:
-        output = response.json()
-        
-        # (April 13, 2024) this is a very janky fix for the token limit issue with a model
-        output = output[:10]
-        
-        return output
-    else:
-        raise Exception(f"Failed to search: {response.status_code} - {response.text}")
+    # extract all NC data for each respected keyword
+    all_results = {}
+    for query in queries:
+        response = requests.get(
+            f"https://nc-api.vercel.app/search?{query}",
+            headers={"Authorization": auth_token},
+        )
+        if response.ok:
+            all_results[query] = response.json()
+
+    # sort by source for each keyword data output
+    sourced_all_results = {}
+    for key in all_results:
+        if key not in sourced_all_results:
+            sourced_all_results[key] = {}
+        for entry in all_results[key]:
+            source = entry["source"]
+            if source not in sourced_all_results[key]:
+                sourced_all_results[key][source] = []
+            sourced_all_results[key][source].append(entry)
+
+    # distribute entry selection across sources to fill output without exceeding limit per source or total
+    output = []
+    existing_ids = []
+    total_entries = sum(len(entries) for keyword in sourced_all_results for source, entries in sourced_all_results[keyword].items())
+    entry_limit = max(1, n // max(1, total_entries))
+    for keyword in sourced_all_results:
+        for source in sourced_all_results[keyword]:
+            count = 0
+            for entry in sourced_all_results[keyword][source]:
+                if len(output) < n and count < entry_limit:
+                    new_id = entry["id"]
+                    if new_id not in existing_ids:
+                        existing_ids.append(new_id)
+                        entry["keyword"] = keyword
+                        output.append(entry)
+                    count += 1
+    
+    # clean up final output to help reduce token count and only keep the information that matters
+    keys_to_remove = {'id', 'source', 'title'}
+    filtered_data = [{k: v for k, v in d.items() if k not in keys_to_remove} for d in output]
+    for item in filtered_data:
+        date = f"{datetime.datetime.utcfromtimestamp(item['recorded']).strftime('%Y-%m-%d %H:%M:%S')} UTC"
+        del item['recorded']
+        item["date"] = date
+    
+    return filtered_data
 
 
 @tool("NmapTool")
