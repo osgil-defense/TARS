631922de26db78f4ed79741553822a965e98e257
mehmet-mhy <mehmet.mhy@gmail.com>
2024-04-26 04:00:15 -0600

    created prompt_route() because semantic-routing is too complex/difficult-to-prefect

--- tars/main.py
+++ tars/main.py
@@ -1,10 +1,12 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+import sys
 
-ChatOpenAI(model="gpt-4-turbo-2024-04-09", temperature=0)
 
-classification_template = PromptTemplate.from_template(
-    """You are good at classifying a question.
+def prompt_route(model_name, prompt_template_params, user_input):
+    # one example prompt that would be generated:
+    """
+    You are good at classifying a question.
     Given the user question below, classify it as either being about 'Car', 'Restaurant', or 'Technology'.
 
     <If the question is about car, mechanics, models, automative technolgoy, classify it as 'Car'>
@@ -13,14 +15,59 @@ classification_template = PromptTemplate.from_template(
     <If the question does not fit any of the classifications, classify it as 'None'>
 
     <question>
-    {question}
+    How do I fix a wheel?
     <question>
 
-    Classifgication:"""
-)
+    Classifgication:
+    """
 
-classification_chain = classification_template | ChatOpenAI()
+    ChatOpenAI(model=model_name, temperature=0)
 
-classification = classification_chain.invoke({"question": str(input("PROMPT: "))})
+    # generate classfication listing for prompt
+    main_list = "Given the user question below, classify it as either being about "
+    carrot_list = ""
+    i = 0
+    d = prompt_template_params["options"]
+    for option in d:
+        main_list += f"'{option}'"
+        if i != len(d) - 1:
+            main_list += ", "
+        else:
+            main_list += "."
+        tmp = ",".join(d[option][:-1] + ["or " + d[option][-1]]).replace(",", ", ")
+        carrot_list += f"<If the question is about {tmp}, classify it as '{option}'>\n"
+        i += 1
+    carrot_list += f"<If the question does not fit any of the classifications, classify it as '{prompt_template_params['default_none']}'>"
 
-print(classification.content)
+    classification_chain = PromptTemplate.from_template(
+        """You are good at classifying a question.
+        {main_list}
+
+        {carrot_list}
+
+        <question>
+        {question}
+        <question>
+
+        Classifgication:"""
+    ) | ChatOpenAI()
+
+    classification = classification_chain.invoke(
+        {"question": user_input, "main_list": main_list, "carrot_list": carrot_list}
+    )
+
+    return classification.content
+
+
+# main function calls
+cmodel = "gpt-4-turbo-2024-04-09"
+coptions = {
+    "default_none": "None",
+    "options": {
+        "Car": ["car", "mechanics", "models", "automative technolgoy"],
+        "Restaurant": ["cuisines", "dining experiences", "restaurant services"],
+        "Technology": ["gadgets", "software", "technological trends"],
+    },
+}
+cout = prompt_route(cmodel, coptions, input("PROMPT: "))
+print(cout)
